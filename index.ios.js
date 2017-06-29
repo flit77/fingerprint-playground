cadf7b1b3700122b100e02d657edb7f253dab5d0
Vasiliy Shubko <flit77@gmail.com>
2017-06-30 00:29:13 +0200

    websockets emit events added

--- index.ios.js
+++ index.ios.js
@@ -4,15 +4,35 @@ import TouchID from "react-native-touch-id"
 
 class Fingerprints extends Component {
   componentDidMount() {
-    TouchID.authenticate("Trying TouchID")
-      .then(success => {
-        alert("Success")
-      })
-      .catch(error => {
-        alert("Failure")
-      })
-  }
+    const socket = new WebSocket("ws://127.0.0.1:8080/ws")
 
+    socket.addEventListener("message", (e) => {
+      const data = JSON.parse(e.data)
+
+      if (data.type === "scan") {
+        TouchID.authenticate("Trying TouchID")
+          .then(success => {
+            // alert("Success")
+
+            socket.send(JSON.stringify({
+              "type": "success",
+              "data": success,
+              "promise": data.promise
+            }))
+          })
+          .catch(error => {
+            // alert("Failure")
+
+            socket.send(JSON.stringify({
+              "type": "error",
+              "data": error,
+              "promise": data.promise
+            }))
+          })
+      }
+    })
+  }
+  
   render() {
     return null
   }
